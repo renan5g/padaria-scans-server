0e7aa376488a6713e3d839d2fbb557fd8514bf5b
baianinho <renansilva.dev5g@gmail.com>
2022-01-02 20:26:40 -0300

    feat: create user resolver

--- src/modules/user/graphql/user.resolver.ts
+++ src/modules/user/graphql/user.resolver.ts
@@ -1,4 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 
-@Resolver()
-export class UserResolver {}
+import { UserService } from '@modules/user/services';
+import { CreateUserInput } from '@modules/user/dtos';
+import { User } from '@modules/user/interfaces';
+
+@Resolver('User')
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+    const result = await this.userService.create(data);
+    return result;
+  }
+}
