51f7d597cad3a2fbe9926074481812e472822a53
baianinho <renansilva.dev5g@gmail.com>
2022-01-02 18:46:27 -0300

    feat: create user resolver

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserService } from '@modules/user/services';
+import { UserResolver } from '@modules/user/graphql';
 
 @Module({
-  providers: [UserService],
+  providers: [UserService, UserResolver],
 })
 export class UserModule {}
