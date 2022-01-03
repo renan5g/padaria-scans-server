05daad5e9f3338f32b39536a24af8a13021b66cc
baianinho <renansilva.dev5g@gmail.com>
2022-01-02 21:39:58 -0300

    feat: CRUD user graphql

--- src/modules/user/graphql/user.resolver.ts
+++ src/modules/user/graphql/user.resolver.ts
@@ -1,16 +1,43 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { UserService } from '@modules/user/services';
-import { CreateUserInput } from '@modules/user/dtos';
+import { CreateUserInput, UpdateUserInput } from '@modules/user/dtos';
 import { User } from '@modules/user/interfaces';
 
 @Resolver('User')
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  @Query(() => [User])
+  async allUsers(): Promise<User[]> {
+    const result = await this.userService.index();
+    return result;
+  }
+
+  @Query(() => User)
+  async showUser(@Args('id') id: string): Promise<User> {
+    const result = await this.userService.show(id);
+    return result;
+  }
+
   @Mutation(() => User)
   async createUser(@Args('data') data: CreateUserInput): Promise<User> {
-    const result = await this.userService.create(data);
+    const result = await this.userService.store(data);
+    return result;
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') id: string,
+    @Args('data') data: UpdateUserInput,
+  ): Promise<User> {
+    const result = await this.userService.update(id, data);
+    return result;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteUser(@Args('id') id: string): Promise<boolean> {
+    const result = await this.userService.destroy(id);
     return result;
   }
 }
--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+
 import { CryptService } from '@modules/global';
-import { CreateUserInput } from '../dtos';
-import { UserRepository } from '../repositories';
+import { message } from '@common/utils';
+
+import { CreateUserInput, UpdateUserInput } from '@modules/user/dtos';
+import { UserRepository } from '@modules/user/repositories';
 
 @Injectable()
 export class UserService {
@@ -10,17 +17,42 @@ export class UserService {
     private readonly cryptService: CryptService,
   ) {}
 
-  async create({ email, username, password }: CreateUserInput) {
+  async store({ email, username, password }: CreateUserInput) {
     const userAlreadyExits = await this.usersRepository.exits(email);
 
-    if (userAlreadyExits) throw new Error('User already exists');
+    if (userAlreadyExits) throw new BadRequestException('User already exists');
 
     const hashedPassword = await this.cryptService.encrypt(password);
 
-    return await this.usersRepository.save({
+    const user = await this.usersRepository.save({
       email,
       password: hashedPassword,
       username,
     });
+
+    return user;
+  }
+
+  async index() {
+    const users = await this.usersRepository.find();
+    return users;
+  }
+
+  async show(id: string) {
+    const user = await this.usersRepository.findById(id);
+
+    if (!user) throw new NotFoundException(message.account_not_found);
+
+    return user;
+  }
+
+  async update(id: string, data: UpdateUserInput) {
+    const user = await this.usersRepository.update(id, data);
+    return user;
+  }
+
+  async destroy(id: string) {
+    const deleted = await this.usersRepository.delete(id);
+    return deleted;
   }
 }
