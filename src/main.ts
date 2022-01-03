553f2c2fb844e247da684281f119de33f7455585
baianinho <renansilva.dev5g@gmail.com>
2022-01-02 21:18:54 -0300

    feat: add validate pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +6,9 @@ const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000, () => logger.log('Server is running ⚡'));
 }
 bootstrap();
